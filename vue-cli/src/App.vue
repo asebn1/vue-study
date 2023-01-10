c866823c7e020ffc427c94dba8843c0d4a633fd9
jero.k <jero.k@kakaocorp.com>
2023-01-11 04:47:47 +0900

    feat: cli를 통한 싱글 파일 컴포넌트 수행

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- 최상위 div는 하나만 존재한다. -->
+  <div>
+    <app-header
+      v-bind:propsdata="str" 
+      v-on:renew="renewStr"
+     ></app-header> 
+  </div>
+</template>
+
+<script>
+// 1. 들고오기
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  // data: {}가 아니라 function으로 진행해야한다.
+  data: function() {
+    return {
+      str: "Header"
+    }
+  },
+  components: {
+    'app-header': AppHeader // 2. 등록하기
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1> {{ propsdata }}</h1>
+        <!-- 하위 -> 상위 이벤트 날리기 -->
+        <button v-on:click="sendEvent">send</button>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function(){
+            this.$emit('renew')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style> 
